d9490209a42c757fe3ec21b81d93817e8c32a355
Cameron <jcmaci03@gmail.com>
2016-03-28 16:05:45 -0400

    Add base plugin file.

---
+++ wp-transient-demo.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Plugin Name: WP Transient Demo
+ * Plugin URI: https://github.com/cambot/wp-transient-demo
+ * Description: Getting to know WordPress Transients.
+ * Version: 0.1
+ * Author: Cameron Macintosh
+**/
+
+defined( 'ABSPATH' ) or die( 'No script kiddies please!');
+
