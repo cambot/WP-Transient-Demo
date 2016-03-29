6e65e94e969885717f0a698f5b0f698fa351b156
Cameron <jcmaci03@gmail.com>
2016-03-28 20:02:15 -0400

    Add Admin shell page.

--- wp-transient-demo.php
+++ wp-transient-demo.php
@@ -9,3 +9,30 @@
 
 defined( 'ABSPATH' ) or die( 'No script kiddies please!');
 
+class transientDemo {
+	function __construct() {
+		add_action( 'init', array( &$this, 'init'));
+	}
+
+	function init() {
+		add_action( 'admin_menu', array( &$this, 'admin_menu'));
+	}
+
+	function admin_menu() {
+		add_menu_page( 'My Little Transient',
+			'Transient Demo',
+			'administrator',
+			'transient-demo',
+			array( &$this, 'page'),
+			'dashicons-admin-site',
+			75
+		);
+	}
+
+	function page() {
+		echo "<h1>My Little Transient</h1>";
+		echo "<p>Trying out WordPress Transients.</p>";
+	}
+}
+
+new transientDemo(); 
