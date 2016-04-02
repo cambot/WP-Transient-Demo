e5150b64156bc3aa5c1f6128eeaa217da679d15b
Cameron <jcmaci03@gmail.com>
2016-04-01 22:49:52 -0400

    Add meetup_api class.

--- wp-transient-demo.php
+++ wp-transient-demo.php
@@ -9,6 +9,46 @@
 
 defined( 'ABSPATH' ) or die( 'No script kiddies please!');
 
+/**
+ * @see www.meetup.com/meetup_api/docs/
+ * Meetup api v3.
+**/
+class meetup_api {
+
+	private $urlname;
+	private $last_response;
+
+	function __construct($urlname) {
+		$this->urlname = $urlname;
+	}
+
+	public function get_urlname() {
+		return $this->urlname;
+	}
+
+	public function get_last_response($response) {
+		return $this->last_response;
+	}
+	public function set_last_response($response) {
+		$this->last_response = $response;
+		return $this;
+	}
+
+	public function get_events() {
+		$url = "https://api.meetup.com/{$this->get_urlname()}/events";
+		// $response is an array with 'body' and 'header' (or WP_Error instance)
+		$response = wp_remote_get($url);
+		if ( is_array($response) && !is_wp_error($response) ) {
+			$this->set_last_response($response);
+			return $response['body'];
+		}
+		if ( is_wp_error($response) ) {
+			return false;
+		}
+		return array();
+	}	
+}
+
 class transientDemo {
 	function __construct() {
 		add_action( 'init', array( &$this, 'init'));
